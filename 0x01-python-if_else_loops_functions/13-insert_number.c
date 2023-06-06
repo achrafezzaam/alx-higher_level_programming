f8e7fd40271917c9b73e54a03b31591edab93a55
achrafezzaam <achraf.ezzaam@gmail.com>
2023-06-06 20:10:13 +0100

    Handling the empty linked list case

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,6 +12,11 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new, *buff = *head;
 
+	if (buff == NULL)
+	{
+		add_nodeint_end(head, number);
+		return (buff);
+	}
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
