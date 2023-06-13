be5757ebc5cee31315a5c0172ddf9e216e023612
achrafezzaam <achraf.ezzaam@gmail.com>
2023-06-13 19:26:45 +0100

    Adding the script for the basic list info. Test needed

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <listobject.h>
+#include <object.h>
+#include <stdio.h>
+/**
+ * print_python_list_info - Print basic info of a list
+ * @p: list to get the info of
+ */
+void print_python_list_info(PyObject *p)
+{
+	int size, i, n;
+	PyObject *elem;
+
+	size = Py_SIZE(p);
+	n = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", n);
+
+	for (i = 0; i < size; i++)
+	{
+		elem = Pylist_GetItel(p, i);
+		printf("Element %d: %s\n", i, Py_TYPE(obj)->tp_name);
+	}
+}
