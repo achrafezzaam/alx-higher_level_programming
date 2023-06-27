ea9be1917b409c061b5a947e65e3d3cbb26aca99
achrafezzaam <achraf.ezzaam@gmail.com>
2023-06-27 23:25:07 +0100

    Adding the script for the Magic task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-class MagicClass:
-    def __init__(self):
-        self._MagicClass__radius = 0
+'''Define the MagicClass class'''
 
-import dis
-dis.dis(MagicClass.__init__)
+
+class MagicClass:
+    '''Create the MagicClass object'''
+    def __init__(self, radius=0):
+        '''Instantiate the MagicClass object
+
+        Args:
+            radius (int or float): The radius of the Disk
+        '''
+        self.__radius = 0
+        if (type(radius) is not int or type(radius) is not float):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        '''Computes the area of the Disk'''
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        '''Computes the circumference of the Disk'''
+        return (2 * math.py * self.__radius)
