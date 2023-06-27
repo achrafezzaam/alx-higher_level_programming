c236d64dc5871a5e147eeb9a6dc2e7ac493dc7de
achrafezzaam <achraf.ezzaam@gmail.com>
2023-06-27 23:34:35 +0100

    Correcting the if statement

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,7 +12,7 @@ class MagicClass:
             radius (int or float): The radius of the Disk
         '''
         self.__radius = 0
-        if (type(radius) is not int or type(radius) is not float):
+        if (type(radius) is not int and type(radius) is not float):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
