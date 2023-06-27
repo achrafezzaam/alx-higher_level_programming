7fb1efbde7730bc9f9ef00602776d20e61021735
achrafezzaam <achraf.ezzaam@gmail.com>
2023-06-27 22:23:50 +0100

    Mondatory tasks done and tested. Still in need of testing for the styling of the comments

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    '''Create a Square object'''
+    def __init__(self, size):
+        '''Instantiate the Square object with value size
+
+        Args:
+            size: the size argument of the Square object
+        '''
+        self.__size = size
