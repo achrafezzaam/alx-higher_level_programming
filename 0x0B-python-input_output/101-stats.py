c02a6c41cace0937cdfa722b455b249aaf970fbf
achrafezzaam <achraf.ezzaam@gmail.com>
2023-07-11 22:02:15 +0100

    Styling changes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Define the stats and get_info method'''
+from sys import stdin
 
 
 def stats(file_size, stats_dict):
@@ -16,15 +17,14 @@ def stats(file_size, stats_dict):
 
 
 if __name__ == "__main__":
-    import sys
 
+    count = 0
     size = 0
     codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     status_dict = {}
-    count = 0
 
     try:
-        for line in sys.stdin:
+        for line in stdin:
             if count == 10:
                 stats(size, status_dict)
                 count = 1
@@ -32,12 +32,10 @@ if __name__ == "__main__":
                 count += 1
 
             line = line.split()
-
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
-
             try:
                 if line[-2] in codes:
                     if status_dict.get(line[-2], -1) == -1:
@@ -46,9 +44,7 @@ if __name__ == "__main__":
                         status_dict[line[-2]] += 1
             except IndexError:
                 pass
-
         stats(size, status_dict)
-
     except KeyboardInterrupt:
         stats(size, status_dict)
         raise
