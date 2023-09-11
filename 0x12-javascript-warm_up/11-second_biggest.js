bd2469655cd831a1d816aedee7dae9afb2186e52
achrafezzaam <achraf.ezzaam@gmail.com>
2023-09-11 18:59:57 +0100

    Changing the sorting script to take into account negative numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length > 3) {
-  const save = process.argv.map(Number).slice(2, process.argv.length).sort();
+  const save = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
   console.log(save[save.length - 2]);
 } else {
   console.log(0);
