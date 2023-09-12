e6c485c118c3d90f782652f883607760264f167a
achrafezzaam <achraf.ezzaam@gmail.com>
2023-09-12 15:48:52 +0100

    Reversing the list without the reverse method

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return (list.reverse());
+  return list.reduceRight(function (previous, current) {
+    previous.push(current);
+    return previous;
+  }, []);
 };
